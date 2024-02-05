9ebf904eaa0576f9d3251d0c3a7400d39d57d983
kappa-git <kevin.borgia@outlook.it>
2024-02-05 14:39:27 +0100

    update 05.02.24

---
+++ src/SpringBoot/ex1/src/main/java/org/develhope/ex1/Ex1Controller.java
@@ -0,0 +1,15 @@
+package org.develhope.ex1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/v1")
+@RestController
+public class Ex1Controller {
+    @GetMapping("/Ciao")
+    public String ciao(@RequestParam String nome, @RequestParam String citta){
+        return "Ciao " + nome + " com'è il tempo a " + citta;
+    }
+}
