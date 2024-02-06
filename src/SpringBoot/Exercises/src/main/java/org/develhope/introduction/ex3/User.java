22a82c7e020b220bef588f7c582c341aed069c81
kappa-git <kevin.borgia@outlook.it>
2024-02-06 16:54:27 +0100

    Update 06.02.24

---
+++ src/SpringBoot/Exercises/src/main/java/org/develhope/introduction/ex3/User.java
@@ -0,0 +1,13 @@
+package org.develhope.introduction.ex3;
+
+public class User {
+    private String nome;
+    private String cognome;
+    private String citta;
+
+    public User(String nome, String cognome, String citta) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.citta = citta;
+    }
+}
