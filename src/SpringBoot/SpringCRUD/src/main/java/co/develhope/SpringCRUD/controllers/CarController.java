0bba5af2004ee18b7b0abf1d4355757ef04818c8
kevin <kevin.borgia@outlook.it>
2024-02-24 21:41:23 +0100

    working progress

--- src/SpringBoot/SpringCRUD/src/main/java/co/develhope/SpringCRUD/controllers/CarController.java
+++ src/SpringBoot/SpringCRUD/src/main/java/co/develhope/SpringCRUD/controllers/CarController.java
@@ -2,11 +2,11 @@ package co.develhope.SpringCRUD.controllers;
 
 import co.develhope.SpringCRUD.etities.Car;
 import co.develhope.SpringCRUD.repository.CarRepository;
+import jakarta.persistence.GeneratedValue;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/car")
@@ -18,6 +18,28 @@ public class CarController {
     public Car createCar(@RequestBody Car car){
         Car carSaved = carRepository.saveAndFlush(car);
         return carSaved;
+    }
 
+    @GetMapping("/all")
+    public List<Car> returnAllCar(){
+        List<Car> carList = carRepository.findAll();
+        return  carList;
+    }
+
+    @GetMapping("/singleCar")
+    public Car singleCar(Car car){
+        if(carRepository.existsById(car.getId()) ){
+            return car;
+        } else {
+            return null;
+        }
+    }
+
+    @GetMapping("/ub")
+    public Car updateModel(Car car){
+        car.setBrand("BMW");
+        return car;
     }
 }
+
+// TODO per ritornare un singolo oggetto car, nell GetMapping usare le graffe per specificare tramite id l'oggeto da ritornare.
