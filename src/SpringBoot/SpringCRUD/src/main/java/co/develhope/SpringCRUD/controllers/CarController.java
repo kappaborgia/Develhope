03065ad8e84442877665859f2f63ac190197e55c
kevin <kevin.borgia@outlook.it>
2024-02-26 22:30:48 +0100

    update

--- src/SpringBoot/SpringCRUD/src/main/java/co/develhope/SpringCRUD/controllers/CarController.java
+++ src/SpringBoot/SpringCRUD/src/main/java/co/develhope/SpringCRUD/controllers/CarController.java
@@ -14,27 +14,27 @@ public class CarController {
     @Autowired
     private CarRepository carRepository;
 
-    @PostMapping
+    @PostMapping // crea un oggetto car
     public Car createCar(@RequestBody Car car){
         Car carSaved = carRepository.saveAndFlush(car);
         return carSaved;
     }
 
-    @GetMapping("/all")
+    @GetMapping("/all") // ritorna tutta la lista di car create in precedenza
     public List<Car> returnAllCar(){
         List<Car> carList = carRepository.findAll();
         return  carList;
     }
 
-    @GetMapping("/singleCar/{id}")
-    //HACK /*
+    @GetMapping("/{id}") //ritorna un sigolo oggetto car specificato tramite la chiamata di rete
+    //REVIEW
     //    public Car singleCar(Car car){
     //        if(carRepository.existsById(car.getId()) ){
     //            return carRepository.findById(car.getId()).orElse(null);
     //        } else {
     //            return new Car();
     //        }
-    //    } */
+    //    }
     public Car singleCar(@PathVariable long id ){
         Car car = carRepository.getById(id);
         if(carRepository.existsById(car.getId())){
@@ -44,14 +44,21 @@ public class CarController {
         }
     }
 
-    //FIXME il metodo restituisce valori null.
-    @GetMapping("/ub/{id}")
-    public Car updateModel(Car car) {
-        if (carRepository.existsById(car.getId())) {
-            car.setBrand("BMW");
-            return car;
-        } else {
-            return new Car();
-        }
+
+    @PutMapping("/{id}") // modifica il tipo di brand di un singolo oggetto car
+    public Car updateModel(@PathVariable long id, @RequestBody Car car) {
+        car.setId(id);
+        Car newCar = carRepository.saveAndFlush(car);
+        return newCar;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable long id){
+        carRepository.deleteById(id);
+    }
+
+    @DeleteMapping
+    public void deleteMultipleById(@RequestParam List<Long> id){
+        carRepository.deleteAllById(id);
     }
 }
