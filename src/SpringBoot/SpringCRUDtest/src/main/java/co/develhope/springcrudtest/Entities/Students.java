ee2c26161900fc3e304be02da2ea9206df582aa5
kappa-git <kevin.borgia@outlook.it>
2024-03-05 19:23:57 +0100

    update

--- src/SpringBoot/SpringCRUDtest/src/main/java/co/develhope/springcrudtest/Entities/Students.java
+++ src/SpringBoot/SpringCRUDtest/src/main/java/co/develhope/springcrudtest/Entities/Students.java
@@ -8,4 +8,51 @@ public class Students {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String surname;
+    @Column(nullable = false)
+    private Boolean isWorking;
+
+    public Students(Long id, String name, String surname, Boolean isWorking) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.isWorking = isWorking;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Boolean getWorking() {
+        return isWorking;
+    }
+
+    public void setWorking(Boolean working) {
+        isWorking = working;
+    }
+
+    public Students() {
+    }
 }
