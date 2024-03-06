a6c92fa12bf77a9d1e651686669013fd60be1a50
kappa-git <kevin.borgia@outlook.it>
2024-03-06 18:51:34 +0100

    update

--- src/SpringBoot/SpringCRUDtest/src/main/java/co/develhope/springcrudtest/Services/StudentService.java
+++ src/SpringBoot/SpringCRUDtest/src/main/java/co/develhope/springcrudtest/Services/StudentService.java
@@ -3,11 +3,15 @@ package co.develhope.springcrudtest.Services;
 import co.develhope.springcrudtest.Entities.Students;
 import co.develhope.springcrudtest.Repositories.StudentsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.NoSuchElementException;
 
-
+@Service
 public class StudentService {
     @Autowired
     StudentsRepository studentsRepository;
@@ -30,6 +34,60 @@ public class StudentService {
             throw new NoSuchElementException("Studente co id:" + id + " non trovato");
         }
     }
+
+    //per aggiornare primary key di uno Student:
+    //passando primary key come path variable
+    //passando Student in request body
+    public Students updateStudent(@PathVariable Long id, @RequestBody Students updateStudent){
+        Students students = studentsRepository.findById(id).orElseThrow(()-> new NoSuchElementException("Studente con id:" + id + " non trovato"));
+        students.setName(updateStudent.getName());
+        students.setSurname(updateStudent.getSurname());
+        students.setWorking(updateStudent.getWorking());
+        studentsRepository.save(students);
+        return students;
+    }
+
+    //per aggiornare il valore isWorking value:
+    //passando primary key come path variable
+    //passando request param con nome working
+    public Students updateAttribute(@PathVariable Long id, @RequestParam Boolean isWorking){
+        Students students = studentsRepository.findById(id).orElseThrow(()-> new NoSuchElementException("Studente con id:" +  id + " non trovato"));
+        students.setWorking(isWorking);
+        studentsRepository.save(students);
+        return students;
+    }
+
+    //per cancellare uno Student
+    public void deleteStudent(@PathVariable Long id){
+        studentsRepository.deleteById(id);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
