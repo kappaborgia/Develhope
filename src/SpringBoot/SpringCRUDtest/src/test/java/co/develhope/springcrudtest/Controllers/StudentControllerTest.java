3e4bbde564619a117acdbfc434523d31ffd6d734
kappa-git <kevin.borgia@outlook.it>
2024-03-15 13:20:55 +0100

    update 15.03.24

---
+++ src/SpringBoot/SpringCRUDtest/src/test/java/co/develhope/springcrudtest/Controllers/StudentControllerTest.java
@@ -0,0 +1,37 @@
+package co.develhope.springcrudtest.Controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class StudentControllerTest {
+
+    @Test
+    void createStudent() {
+    }
+
+    @Test
+    void returnListAllStudents() {
+    }
+
+    @Test
+    void getStudenteById() {
+    }
+
+    @Test
+    void updateAttribute() {
+    }
+
+    @Test
+    void updateIsWorking() {
+    }
+
+    @Test
+    void deleteStudent() {
+    }
+}
