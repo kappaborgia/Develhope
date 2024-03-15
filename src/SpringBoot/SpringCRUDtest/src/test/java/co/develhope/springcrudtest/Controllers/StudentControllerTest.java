7fa588a77b0ccbac0e62098cc3868d9ada0b23e4
kevin <kevin.borgia@outlook.it>
2024-03-15 13:42:40 +0100

    update

--- src/SpringBoot/SpringCRUDtest/src/test/java/co/develhope/springcrudtest/Controllers/StudentControllerTest.java
+++ src/SpringBoot/SpringCRUDtest/src/test/java/co/develhope/springcrudtest/Controllers/StudentControllerTest.java
@@ -3,12 +3,14 @@ package co.develhope.springcrudtest.Controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles(value = "test")
 class StudentControllerTest {
 
     @Test
